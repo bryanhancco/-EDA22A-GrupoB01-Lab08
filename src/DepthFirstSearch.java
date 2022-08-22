2dc4244d6c30d34ca6746e2bf6d8dc9901be857c
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2022-08-21 20:49:17 -0500

    creating visited nodes hashmap

--- src/DepthFirstSearch.java
+++ src/DepthFirstSearch.java
@@ -5,6 +5,11 @@ public class DepthFirstSearch<E> {
 	private Graph<E> grafo;
 	
 	public DepthFirstSearch(Graph<E> grafo) {
+    this.nodosVisitados = new HashMap<Vertice<E>, Boolean>();
+		this.grafo = grafo;
+		for(Vertice<E> vertice : (ArrayList<Vertice<E>>)this.grafo.getVertices()) {
+			this.nodosVisitados.put(vertice, false);
+		}
 	}
 	public void DFS(Vertice<E> vertice) {
 	}
