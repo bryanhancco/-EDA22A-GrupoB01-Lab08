50b5f46ba7deef7ab96a74724cd48381bf84d455
Gabriel <gmamanican@unsa.edu.pe>
2022-08-21 23:11:45 -0500

    adding the test in the same class

--- src/Dijkstra/Dijkstra.java
+++ src/Dijkstra/Dijkstra.java
@@ -4,6 +4,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Dijkstra {
+    public static void main(String[] args) {
+        Dijkstra graph = new Dijkstra(5);
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+        graph.addVertex("E");
+
+        graph.addEdges(0, 1, 10);
+        graph.addEdges(0, 2, 2);
+        graph.addEdges(2, 1, 1);
+        graph.addEdges(2, 4, 7);
+        graph.addEdges(1, 4, 1);
+        graph.addEdges(4, 3, 3);
+
+        showEdges();
+        graph.dijkStra(0);
+
+    }
 
     private int edgesAmount;
     private int n;
