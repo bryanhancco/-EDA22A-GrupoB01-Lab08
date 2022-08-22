8947341842e1d62acab1011af2f3c0089083c703
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2022-08-21 20:47:40 -0500

    adding DFS structure

---
+++ src/DepthFirstSearch.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class DepthFirstSearch<E> {
+	private HashMap<Vertice<E>, Boolean> nodosVisitados;
+	private Graph<E> grafo;
+	
+	public DepthFirstSearch(Graph<E> grafo) {
+	}
+	public void DFS(Vertice<E> vertice) {
+	}
+}
