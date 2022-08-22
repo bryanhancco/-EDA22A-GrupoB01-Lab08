ce6856ad1c9befc38faf6fd977756066d422d2da
CarloDiazP <88125777+CarloDiazP@users.noreply.github.com>
2022-08-21 20:56:18 -0500

    get and toString methods

--- src/Vertice.java
+++ src/Vertice.java
@@ -30,4 +30,12 @@ public class Vertice<E>{
     }
     return false;
   }
+  
+  public ArrayList<Vertice<E>> getAdjacentVertex(){
+	  return this.adjacentVertices;
+  }
+  
+  public String toString() {
+	  return this.data.toString();
+  }
 }
