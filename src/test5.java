1784b9d24934f5542dbab791351414950214750d
Barbara280801 <bcarrillo@unsa.edu.pe>
2022-08-21 23:39:58 -0500

    agrego test5 donde se testea el método content del ejercicio 5

---
+++ src/test5.java
@@ -0,0 +1,24 @@
+public class test5 {
+    public static void main(String[] args) {
+        Graph<String> graph = new Graph<String>();
+        graph.addEdge("fraasasnco", "karen");
+        graph.addEdge("jose", "jesus");
+        graph.addEdge("carlo", "jose");
+        graph.addEdge("barbara", "jose");
+        graph.addEdge("jose", "barbara");
+        graph.addEdge("yerick", "barbie");
+        System.out.println(graph);
+    
+        Graph<String> graph2 = new Graph<String>();
+        graph2.addEdge("fraasasnco", "karen");
+        graph2.addEdge("jose", "jesus");
+        graph2.addEdge("carlo", "jose");
+        graph2.addEdge("barbara", "jose");
+        graph2.addEdge("jose", "barbara");
+        graph.addEdge("yerick", "barbie");
+        System.out.println(graph2);
+    
+        System.out.println(graph.content(graph2, graph));
+    }
+    
+}
